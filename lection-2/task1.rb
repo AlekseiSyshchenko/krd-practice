45ae84948b7a6742a20f956c6966ae789f7e98ba
Aleksei Syshchenko~ <syshenco@gmail.com>
2023-07-19 19:33:07 +0400

    Change method & tests for output in stdout

--- lection-2/task1.rb
+++ lection-2/task1.rb
@@ -1,15 +1,13 @@
-def greeting(name, surname, age)
-  if age < 18
-    "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
-  else
-    "Привет, #{name} #{surname}. Самое время заняться делом!"
-  end
-end
-
-
+def greeting
   puts "Введите ваши Имя Фамилию возраст (через пробел)"
   data = gets.chomp
   name, surname, age = data.split
   age = age.to_i
+  if age < 18
+    puts "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
+  else
+    puts "Привет, #{name} #{surname}. Самое время заняться делом!"
+  end
+end
 
-  puts greeting(name, surname, age)
+greeting()
--- lection-2/task1_spec.rb
+++ lection-2/task1_spec.rb
@@ -5,10 +5,12 @@ require './task1'
 RSpec.describe "Task1" do
   it '#greeting less than 18' do
     allow_any_instance_of(Kernel).to receive(:gets).and_return("Vasya Pirozhkov 15")
-    expect(greeting('Vasya', 'Pirozhkov', 15)).to eq("Привет, Vasya Pirozhkov. Тебе меньше 18 лет, но начать учиться программировать никогда не рано")
+    expect{greeting}.to output("Введите ваши Имя Фамилию возраст (через пробел)\nПривет, Vasya Pirozhkov. Тебе меньше 18 лет, но начать учиться программировать никогда не рано\n").to_stdout
   end
+
   it '#greeting more than 18' do
     allow_any_instance_of(Kernel).to receive(:gets).and_return("John Snow 38")
-    expect(greeting('John', 'Snow', 38)).to eq("Привет, John Snow. Самое время заняться делом!")
+    expect{greeting}.to output("Введите ваши Имя Фамилию возраст (через пробел)\nПривет, John Snow. Самое время заняться делом!\n").to_stdout
+
   end
 end
